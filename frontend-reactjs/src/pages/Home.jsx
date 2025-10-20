d5ce2d6a9968bc1093677de848c73927ab84e549
orbas1 <ironcrest96@gmail.com>
2025-10-20 20:29:46 +0100

    Add localized home preview tabs

--- frontend-reactjs/src/pages/Home.jsx
+++ frontend-reactjs/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import HomeHero from '../components/home/HomeHero.jsx';
 import FeatureGrid from '../components/FeatureGrid.jsx';
+import InsidePreviewTabs from '../components/home/InsidePreviewTabs.jsx';
 import Testimonials from '../components/Testimonials.jsx';
 
 export default function Home() {
@@ -7,6 +8,8 @@ export default function Home() {
     <div className="bg-slate-50 text-slate-900">
       <HomeHero />
       <FeatureGrid />
+      {/* Tutor spotlight section renders above this component when available. */}
+      <InsidePreviewTabs />
       <Testimonials />
     </div>
   );
